9e5a9c47a723162a4765c2283d41e3a080db7193
manoj bhatt <enterbhatt123@gmail.com>
2025-06-28 12:25:45 +0530

    i changed the toggle state using basic useState hook... next will be using redux and see how to work with diffrent data

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,16 @@
 import classes from './Counter.module.css';
 import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 const Counter = () => {
   const dispatch = useDispatch();
+  const [show, setShow] = useState(true);
   const counter = useSelector(state => state.counter);//acts as subscription to the redux store
 
   const increaseHandler = ()=>{
     dispatch({type:'increase', amount:5})//this is the custom handler we can also take different 
     // values from the input without hardcoding the reducer function
   }
-
+  
   const incrementHandler = ()=>{
     console.log("clicked")
     dispatch({type: 'increment'})
@@ -19,13 +21,18 @@ const Counter = () => {
   }
 
   const toggleCounterHandler = () => {
-
+    if(show === true){
+      setShow(false);
+    }
+    if(show === false){
+      setShow(true)
+    }
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>
           increment
